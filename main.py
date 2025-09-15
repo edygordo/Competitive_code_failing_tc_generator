2c4619ce870a78edb4949aaacf07ab4788efe37c
edygordo <aditya.sahu.96@gmail.com>
2025-09-16 00:44:07 +0530

    Added dummy input dictionary

--- main.py
+++ main.py
@@ -10,4 +10,7 @@ from graph.graph import app
 if __name__ == "__main__":
     print("Test Case Simplifier App")
 
-    print(app.invoke({}))
+    print(app.invoke({"question": "your question here",
+                      "user_code": "user code here",
+                      "language": "python",
+                      "baseline_code": "baseline correct code here"}))
